032b699bf82dc5893fdda974a84e674d70fb7bd9
Rohit Wahwal <zeroth.rohit@gmail.com>
2022-02-05 15:44:38 +0530

    Add BMI formula

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
-from readline import remove_history_item
-
+from flask import Flask
 
 print("hi")
 print("hello")
 print("Diet planner")
+
+#BMI calculation
+weight = 50
+height = 175
+BMI = weight/(height**2)
+print("The BMI is",BMI)
+
+#BMR calculation
+
+#Fat percent calculation
+
