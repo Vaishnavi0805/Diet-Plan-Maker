c0f85894fd8f576c1f79f451f62a035eccc1eba3
Vaishnavi0805 <99071024+Vaishnavi0805@users.noreply.github.com>
2022-02-09 23:31:34 +0530

    updated html

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__, static_url_path='/static')
 app.secret_key = "16516516"
 
 
-@app.route('/', methods = ['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def cal():
     if request.method == 'POST':
         age = request.form['age']
@@ -12,60 +12,62 @@ def cal():
         Height = request.form['Height']
         gender = request.form['gender']
         PA = request.form['PA']
-        n=float(Height)
-        w=float(Weight)
-        a=int(age)
+        h = float(Height)
+        w = float(Weight)
+        a = int(age)
+        LFM = 0
         # Age=19
         # Weight=60
         # Height=175
         # gender= "male"
         # PA="Moderate"
-        Hmeter=(n/100)
-        #print(Hmeter)
+        Hmeter = (h/100)
+        # print(Hmeter)
         BMI = w/(Hmeter**2)
         print(BMI)
-        if gender=='female':
-            BFP=(1.20*BMI)+(0.23*a)-5.4
+        if gender == 'female':
+            BFP = (1.20*BMI)+(0.23*a)-5.4
             print(BFP)
-            #Female LFM
+            # Female LFM
             if BFP >= 14 and BFP <= 18:
-                LFM = 1.0
+                LFM = LFM + 1.0
             elif BFP >= 19 and BFP <= 28:
-                LFM = 0.95 
+                LFM = LFM + 0.95
             elif BFP >= 29 and BFP <= 38:
-                LFM = 0.90 
+                LFM = LFM + 0.90
             elif BFP >= 38:
-                LFM = 0.85
-            BMR= w*0.9*24*LFM
-        elif gender=='male':
-            BFP=(1.20*BMI)+(0.23*a)-16.2
+                LFM = LFM + 0.85
+            BMR = w*0.9*24*LFM
+        elif gender == 'male':
+            BFP = (1.20*BMI)+(0.23*a)-16.2
             print(BFP)
-            #Male LFM
+            # Male LFM
             if BFP >= 10 and BFP <= 14:
-                LFM = 1.0 
+                LFM = LFM + 1.0
             elif BFP >= 15 and BFP <= 20:
-                LFM = 0.95 
+                LFM = LFM + 0.95
             elif BFP >= 21 and BFP <= 28:
-                LFM = 0.90 
+                LFM = LFM + 0.90
             elif BFP >= 28:
-                LFM = 0.85 
+                LFM = LFM + 0.85
             # print(LFM)
-        BMR = w*1.0*24*LFM
+            BMR = w*1.0*24*LFM
         # print(BMR)
 
-        if PA=="Very Light":
-            Final_calorie=BMR*1.3
-        elif PA=="Light":
-            Final_calorie=BMR*1.55
-        elif PA=="Moderate":
-            Final_calorie=BMR*1.65
-        elif PA=="Heavy":
-            Final_calorie=BMR*1.80 
-        elif PA=="Very Heavy":
-            Final_calorie=BMR*2.00  
+        if PA == "Very Light":
+            Final_calorie = BMR*1.3
+        elif PA == "Light":
+            Final_calorie = BMR*1.55
+        elif PA == "Moderate":
+            Final_calorie = BMR*1.65
+        elif PA == "Heavy":
+            Final_calorie = BMR*1.80
+        elif PA == "Very Heavy":
+            Final_calorie = BMR*2.00
         print(Final_calorie)
-        return render_template("output.html", final = Final_calorie)
+        return render_template("output.html", final=Final_calorie)
     return render_template('index.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- sample.py
+++ sample.py
@@ -1,10 +1,10 @@
-Age=19
-Weight=60
-Height=175
-gender= "male"
-Hmeter=Height/100
+Age = 19
+Weight = 60
+Height = 175
+gender = "male"
+Hmeter = Height/100
 BMI = Weight/(Hmeter**2)
 
 print(BMI)
-BFP=(1.20*BMI)+(0.23*Age)-16.2
+BFP = (1.20*BMI)+(0.23*Age)-16.2
 print(BFP)
